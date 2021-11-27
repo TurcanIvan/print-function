61fc39e492daafca10ccf4674fc99bf407ebdefb
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:27:40 +0200

    Exemple

---
+++ print-function.py
@@ -0,0 +1,15 @@
+## need a function
+
+## "Hello" ---> [ Hello ]
+## "Bye" --->    [ Bye ] 
+
+### FUNCTION DEFINITION
+def printFormatted( message ):
+    print("---------------------")
+    print("[", message ,"]")
+    print("---------------------")
+
+
+### CALL THE FUNCTION
+printFormatted("Hello!")
+printFormatted("Bye!")
